16ef34dad59e76545deb9e6b974ae2f772d8c459
JuniorProgram9810 <132420442+JuniorProgram9810@users.noreply.github.com>
2025-07-03 12:07:11 -0600

    Delete User.cs

--- User.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PollAventuras.Models;
-public class User
-{
-    public string Id { get; set; }
-    public string Email { get; set; }
-    public string Name { get; set; }
-    public DateTime RegistrationDate { get; set; }
-    public List<string> PurchasedGames { get; set; }
-}
