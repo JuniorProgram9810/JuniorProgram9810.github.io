d31d2a2fb3b3ad5267da590e2b6205a8565809d3
JuniorProgram9810 <132420442+JuniorProgram9810@users.noreply.github.com>
2025-07-03 19:47:52 -0600

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using AppIntegradora10A.Views;
+
+namespace AppIntegradora10A
+{
+    public partial class MainPage : ContentPage
+    {
+        
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        private async void OnAddProduct_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new AddProductPage());
+        }
+
+        private async void OnListProduct_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ListProductPage());
+        }
+
+    }
+
+}
