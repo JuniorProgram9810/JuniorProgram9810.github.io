4d848620576a822ca6e3d0f9557d04fc0dd208ac
JuniorProgram9810 <132420442+JuniorProgram9810@users.noreply.github.com>
2025-08-04 13:26:31 -0600

    Add files via upload

---
+++ AppShell.xaml.cs
@@ -0,0 +1,20 @@
+﻿using AppIntegradora10A.Views;
+
+namespace AppIntegradora10A
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+
+            // Registrar las rutas para navegación
+            Routing.RegisterRoute("LoginPage", typeof(LoginPage));
+            Routing.RegisterRoute("MainPage", typeof(MainPage));
+            Routing.RegisterRoute("AddProductPage", typeof(AddReportePage));
+            Routing.RegisterRoute("ListProductPage", typeof(ListReportePage));
+            Routing.RegisterRoute("EditProductPage", typeof(EditReportePage));
+            
+        }
+    }
+}
